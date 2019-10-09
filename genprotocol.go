424d13648ad6ac2cc9e66ef78dba889dace4abf0
SeukWon Kang <kasworld@gmail.com>
2019-10-09 17:09:16 +0900

    add wasmconn

--- genprotocol.go
+++ genprotocol.go
@@ -106,6 +106,7 @@ func main() {
 		"_obj",
 		"_packet",
 		"_version",
+		"_wasmconn",
 	}
 
 	cmddatafile := path.Join(*basedir, *prefix+"_gendata", "command.data")
@@ -169,6 +170,9 @@ func main() {
 
 	buf, err = buildAPITemplate(*prefix, cmddata, notidata)
 	saveTo(buf, err, path.Join(*basedir, *prefix+"_server", "apitemplate_gen.go"))
+
+	buf, err = buildWasmConn(*prefix)
+	saveTo(buf, err, path.Join(*basedir, *prefix+"_wasmconn", "wasmconn_gen.go"))
 }
 
 func buildDataCode(pkgname string, enumtype string, data [][]string) (*bytes.Buffer, error) {
@@ -835,5 +839,181 @@ func buildAPITemplate(prefix string, cmddata, notidata [][]string) (*bytes.Buffe
 	}
 	fmt.Fprintf(&buf, `
 	*/`)
+	return &buf, nil
+}
+
+func buildWasmConn(prefix string) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+	fmt.Fprintln(&buf, makeGenComment())
+	fmt.Fprintf(&buf, `
+		package %[1]s_wasmconn
+
+		import (
+			"context"
+			"fmt"
+			"sync"
+			"syscall/js"
+		)
+
+		`, prefix)
+
+	fmt.Fprintf(&buf, `
+	type Connection struct {
+		remoteAddr   string
+		conn         js.Value
+		SendRecvStop func()
+		sendCh       chan %[1]s_packet.Packet
+	
+		marshalBodyFn      func(interface{}, []byte) ([]byte, byte, error)
+		handleRecvPacketFn func(header %[1]s_packet.Header, body []byte) error
+		handleSentPacketFn func(header %[1]s_packet.Header) error
+	}
+	
+	func (wsc *Connection) String() string {
+		return fmt.Sprintf("Connection[%%v SendCh:%%v]",
+			wsc.remoteAddr, len(wsc.sendCh))
+	}
+	
+	func New(
+		connAddr string,
+		marshalBodyFn func(interface{}, []byte) ([]byte, byte, error),
+		handleRecvPacketFn func(header %[1]s_packet.Header, body []byte) error,
+		handleSentPacketFn func(header %[1]s_packet.Header) error,
+	) *Connection {
+		wsc := &Connection{
+			remoteAddr:         connAddr,
+			sendCh:             make(chan %[1]s_packet.Packet, 10),
+			marshalBodyFn:      marshalBodyFn,
+			handleRecvPacketFn: handleRecvPacketFn,
+			handleSentPacketFn: handleSentPacketFn,
+		}
+		wsc.SendRecvStop = func() {
+			JsLogErrorf("Too early SendRecvStop call %%v", wsc)
+		}
+		return wsc
+	}
+	
+	func (wsc *Connection) Connect(ctx context.Context, wg *sync.WaitGroup) error {
+		connCtx, ctxCancel := context.WithCancel(ctx)
+		wsc.SendRecvStop = ctxCancel
+	
+		wsc.conn = js.Global().Get("WebSocket").New(wsc.remoteAddr)
+		if !wsc.conn.Truthy() {
+			err := fmt.Errorf("fail to connect %%v", wsc.remoteAddr)
+			JsLogErrorf("%%v", err)
+			return err
+		}
+		wsc.conn.Call("addEventListener", "open", js.FuncOf(
+			func(this js.Value, args []js.Value) interface{} {
+				wsc.conn.Call("addEventListener", "message", js.FuncOf(wsc.handleWebsocketMessage))
+				go wsc.sendLoop(connCtx)
+				wg.Done()
+				return nil
+			}))
+		wsc.conn.Call("addEventListener", "close", js.FuncOf(wsc.wsClosed))
+		wsc.conn.Call("addEventListener", "error", js.FuncOf(wsc.wsError))
+		return nil
+	}
+	
+	func (wsc *Connection) wsClosed(this js.Value, args []js.Value) interface{} {
+		wsc.SendRecvStop()
+		JsLogError("ws closed")
+		return nil
+	}
+	
+	func (wsc *Connection) wsError(this js.Value, args []js.Value) interface{} {
+		wsc.SendRecvStop()
+		JsLogError(this, args)
+		return nil
+	}
+	
+	func (wsc *Connection) sendLoop(sendRecvCtx context.Context) {
+		defer wsc.SendRecvStop()
+		var err error
+	loop:
+		for {
+			select {
+			case <-sendRecvCtx.Done():
+				break loop
+			case pk := <-wsc.sendCh:
+				var sendBuffer []byte
+				sendBuffer, err = %[1]s_packet.Packet2Bytes(&pk, wsc.marshalBodyFn)
+				if err != nil {
+					break loop
+				}
+				if err = wsc.sendPacket(sendBuffer); err != nil {
+					break loop
+				}
+				if err = wsc.handleSentPacketFn(pk.Header); err != nil {
+					break loop
+				}
+			}
+		}
+		JsLogErrorf("end SendLoop %%v\n", err)
+		return
+	}
+	
+	func (wsc *Connection) sendPacket(sendBuffer []byte) error {
+		sendData := js.Global().Get("Uint8Array").New(len(sendBuffer))
+		js.CopyBytesToJS(sendData, sendBuffer)
+		wsc.conn.Call("send", sendData)
+		return nil
+	}
+	
+	func (wsc *Connection) handleWebsocketMessage(this js.Value, args []js.Value) interface{} {
+		data := args[0].Get("data") // blob
+		aBuff := data.Call("arrayBuffer")
+		aBuff.Call("then",
+			js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	
+				rdata := ArrayBufferToSlice(args[0])
+				rPk := %[1]s_packet.NewRecvPacketBufferByData(rdata)
+				header, body, lerr := rPk.GetHeaderBody()
+				if lerr != nil {
+					JsLogError(lerr.Error())
+					wsc.SendRecvStop()
+					return nil
+				} else {
+					if err := wsc.handleRecvPacketFn(header, body); err != nil {
+						JsLogErrorf("%%v", err)
+						wsc.SendRecvStop()
+						return nil
+					}
+				}
+				return nil
+			}))
+	
+		return nil
+	}
+	
+	func Uint8ArrayToSlice(value js.Value) []byte {
+		s := make([]byte, value.Get("byteLength").Int())
+		js.CopyBytesToGo(s, value)
+		return s
+	}
+	
+	func ArrayBufferToSlice(value js.Value) []byte {
+		return Uint8ArrayToSlice(js.Global().Get("Uint8Array").New(value))
+	}
+	
+	func (wsc *Connection) EnqueueSendPacket(pk %[1]s_packet.Packet) error {
+		select {
+		case wsc.sendCh <- pk:
+			return nil
+		default:
+			return fmt.Errorf("Send channel full %%v", wsc)
+		}
+	}
+	
+	/////////
+	
+	func JsLogError(v ...interface{}) {
+		js.Global().Get("console").Call("error", v...)
+	}
+	
+	func JsLogErrorf(format string, v ...interface{}) {
+		js.Global().Get("console").Call("error", fmt.Sprintf(format, v...))
+	}
+		`, prefix)
 	return &buf, nil
 }
